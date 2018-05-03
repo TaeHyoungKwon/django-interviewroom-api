6b9df024261921f0aeaaeb3fe099a9f76140af5e
thkwon <TaeHYoungKwon@github.com>
2018-05-03 10:47:05 +0900

    Refactor: Update UI Design and Logic(creating times object)

--- Frontend/TimeList/TimeList.js
+++ Frontend/TimeList/TimeList.js
@@ -9,81 +9,120 @@ export default class TimeList extends React.Component {
   constructor(props) {
     super(props);
 
-    var today = new Date();
-    var year = today.getFullYear();
-    var month = today.getMonth() + 1;
-    var date = today.getDate();
-    var hours = today.getHours();
-    var minutes = today.getMinutes();
-
-    var resultTime =
-      year +
-      '년 ' +
-      month +
-      '월 ' +
-      date +
-      '일 ' +
-      hours +
-      '시 ' +
-      minutes +
-      '분';
-
     this.state = {
       datas: [],
       times: [
-        '09:00~09:30',
-        '09:30~10:00',
-        '10:00~10:30',
-        '10:30~11:00',
-        '11:00~11:30',
-        '11:30~12:00',
-        '12:00~12:30',
-        '12:30~13:00'
+        {
+          index: 0,
+          p_start_time: '09:00',
+          p_end_time: '09:30'
+        },
+        {
+          index: 1,
+          p_start_time: '09:30',
+          p_end_time: '10:00'
+        },
+        {
+          index: 2,
+          p_start_time: '10:00',
+          p_end_time: '10:30'
+        },
+        {
+          index: 3,
+          p_start_time: '10:30',
+          p_end_time: '11:00'
+        },
+        {
+          index: 4,
+          p_start_time: '11:00',
+          p_end_time: '11:30'
+        },
+        {
+          index: 5,
+          p_start_time: '11:30',
+          p_end_time: '12:00'
+        },
+        {
+          index: 6,
+          p_start_time: '12:00',
+          p_end_time: '12:30'
+        },
+        {
+          index: 7,
+          p_start_time: '12:30',
+          p_end_time: '13:00'
+        },
+        {
+          index: 8,
+          p_start_time: '13:00',
+          p_end_time: '13:30'
+        },
+        {
+          index: 9,
+          p_start_time: '13:30',
+          p_end_time: '14:00'
+        },
+        {
+          index: 10,
+          p_start_time: '14:00',
+          p_end_time: '14:30'
+        },
+        {
+          index: 11,
+          p_start_time: '14:30',
+          p_end_time: '15:00'
+        },
+        {
+          index: 12,
+          p_start_time: '15:00',
+          p_end_time: '15:30'
+        },
+        {
+          index: 13,
+          p_start_time: '15:30',
+          p_end_time: '16:00'
+        },
+        {
+          index: 14,
+          p_start_time: '16:00',
+          p_end_time: '16:30'
+        },
+        {
+          index: 15,
+          p_start_time: '16:30',
+          p_end_time: '17:00'
+        },
+        {
+          index: 16,
+          p_start_time: '17:00',
+          p_end_time: '17:30'
+        },
+        {
+          index: 17,
+          p_start_time: '17:30',
+          p_end_time: '18:00'
+        },
+        {
+          index: 18,
+          p_start_time: '02:00',
+          p_end_time: '02:30'
+        },
+        {
+          index: 19,
+          p_start_time: '02:30',
+          p_end_time: '03:00'
+        }
       ],
-      isIndicator: true,
-      curTime: resultTime
+      isIndicator: true
     };
   }
 
   componentDidMount() {
     this.getListData();
-
-    this.intervalID = setInterval(() => this.tick(), 60000);
-  }
-  componentWillUnmount() {
-    clearInterval(this.intervalID);
-  }
-
-  tick() {
-    var today = new Date();
-    var year = today.getFullYear();
-    var month = today.getMonth() + 1;
-    var date = today.getDate();
-    var hours = today.getHours();
-    var minutes = today.getMinutes();
-
-    var resultTime =
-      year +
-      '년 ' +
-      month +
-      '월 ' +
-      date +
-      '일 ' +
-      hours +
-      '시 ' +
-      minutes +
-      '분';
-
-    this.setState({
-      curTime: resultTime
-    });
   }
 
   getListData() {
-    console.log('TimeList');
-    console.log(this.props.pk);
-
-    return fetch('http://192.168.1.68:8001/api/time_list/' + this.props.pk)
+    return fetch('http://10.2.236.67:8001/api/time_list/' + this.props.pk)
       .then(response => response.json())
       .then(responseJson => {
         console.log(responseJson);
@@ -100,24 +139,56 @@ export default class TimeList extends React.Component {
   }
 
   render() {
-    console.log(this.state.datas);
-    let lists = this.state.datas.map((data, index) => {
-      const { room, start_time, end_time, interviewee, interviewer, pk } = data;
+    let lists = this.state.times.map((times, index) => {
+      let {
+        p_start_time,
+        p_end_time,
+        p_interviewee,
+        fullStartTime,
+        fullEndTime
+      } = times;
+
+      this.state.datas.map((data, index) => {
+        let { start_time, end_time, interviewee } = data;
+
+        let new_start_time = start_time.split('T')[1].slice(0, 5);
+        let new_end_time = end_time.split('T')[1].slice(0, 5);
+
+        if (p_start_time == new_start_time || p_end_time == new_end_time) {
+          console.log('-----------');
+          console.log(p_start_time);
+          console.log(p_end_time);
+          console.log('-----------');
+          p_interviewee = interviewee;
+          fullStartTime = start_time;
+          fullEndTime = end_time;
+        }
+      });
 
       return (
         <InterviewTime
-          key={pk}
-          pk={pk}
-          room={room}
-          startTime={start_time}
-          endTime={end_time}
-          interviewee={interviewee}
-          interviewer={interviewer}
+          startTime={p_start_time}
+          endTime={p_end_time}
+          interviewee={p_interviewee}
           name={this.props.name}
-          curTime={this.state.curTime}
+          fullStartTime={fullStartTime}
+          fullEndTime={fullEndTime}
         />
       );
     });
+
+    // let lists = this.state.datas.map((data, index) => {
+    //   const { room, start_time, end_time, interviewee} = data;
+
+    //   return (
+    //     <InterviewTime
+    //       startTime={start_time}
+    //       endTime={end_time}
+    //       interviewee={interviewee}
+    //       name={this.props.name}
+    //     />
+    //   );
+    // });
 
     return (
       <View style={[styles.container]}>
